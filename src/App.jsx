67ce75e9c51540ace2f6e61b75f68ed63c3d526b
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2024-10-02 21:11:02 -0300

    corrigindo arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
 import { useState } from "react";
-
 import Perfil from "./components/Perfil";
 import Formulario from "./components/Formulario";
 import ReposList from "./components/ReposList";
 
 function App() {
   const [FormularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
-  const [nomeUsuario, setNomeUsuario] = useState('')
+  const [nomeUsuario, setNomeUsuario] = useState('');
+  const [erro, setErro] = useState('');
+
+  const handleNomeUsuarioChange = (e) => {
+    const valor = e.target.value;
+    setNomeUsuario(valor);
+    
+    // Validação simples
+    if (valor.length < 4) {
+      setErro("O nome de usuário deve ter pelo menos 4 caracteres.");
+    } else {
+      setErro('');
+    }
+  };
 
   return (
     <>
-    <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)}/>
+      <input 
+        type="text" 
+        className="searchInput" 
+        onBlur={handleNomeUsuarioChange} 
+        placeholder="Digite seu nome de usuário no GitHub" 
+      />
+      {erro && <p className="error">{erro}</p>}
 
-    {nomeUsuario.length > 4 && (
-      <>
-        <Perfil nomeUsuario={nomeUsuario}/>
-        <ReposList nomeUsuario={nomeUsuario} />
-      </>
-    )}
-    
-    {/* {FormularioEstaVisivel && (
-      <Formulario />
-    )}
-
-    <button onClick={() => setFormularioEstaVisivel(!FormularioEstaVisivel)} type="button">toggle form</button> */}
+      {nomeUsuario.length >= 4 && (
+        <>
+          <Perfil nomeUsuario={nomeUsuario} />
+          <ReposList nomeUsuario={nomeUsuario} />
+        </>
+      )}
+      
+      {/* {FormularioEstaVisivel && <Formulario />}
+      <button 
+        onClick={() => setFormularioEstaVisivel(!FormularioEstaVisivel)} 
+        type="button"
+      >
+        Toggle Formulário
+      </button> */}
     </>
-  )
-
+  );
 }
 
-export default App
+export default App;
